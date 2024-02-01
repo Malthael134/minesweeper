8e85f32d8a09875fe6b8a6775327d5090e72cb43
Malthael134 <127251883+Malthael134@users.noreply.github.com>
2024-02-01 18:11:40 +0100

    Progress & Docker image build

--- src/lib/Minesweeper/Board.svelte
+++ src/lib/Minesweeper/Board.svelte
@@ -1,38 +1,129 @@
 <script>
-	import { boardSizeStore } from "../../stores";
-    
-    /** @type {number[]} rows */
-    $: rows = Array.from(
-        {length: $boardSizeStore},
-        (_, i) => i
-    );
-    
-    /** @type {number[]} columns */
-    $: columns = Array.from(
-        {length: $boardSizeStore},
-        (_, i) => i
-    );
 
-    
+import { boardSizeStore, mineCountStore } from "../../stores";
+
+/** @type {string[]} */
+let mineLocations = [];
+
+/** @type {number} */
+let tilesClicked = 0;
+
+/** @type {Tile[][]} */
+let board = [];
+
+/**
+ * @class
+ */
+class Tile {
+    /** 
+     * @constructor
+     * @param {string} prop_id
+     * @param {number} prop_row
+     * @param {number} prop_column
+     * @param {string} prop_text
+     */
+    constructor(prop_id, prop_row, prop_column, prop_text) {
+        
+        /** @member {string} */
+        this.id = prop_id;
+
+        /** @member {string} */
+        this.class = `tile ${mineLocations.includes(this.id) ? "bomb" : ""}`;
+
+        /** @member {number} */
+        this.row = prop_row;
+
+        /** @member {number} */
+        this.column = prop_column;
+
+        /** @member {string} */
+        this.text = prop_text || "";
+
+    }
+}
+
+/* Start game */
+function startGame() {
+    setMines();
+    generateBoard();
+}
+
+
+function generateBoard() {
+    board = [];
+    for (let r = 0; r < $boardSizeStore; ++r) {
+        board.push([]);
+        for (let c = 0; c < $boardSizeStore; ++c) {
+            board[r].push(new Tile( `${r}-${c}`, r, c, ''));
+        }
+    }
+    board = board;
+}
+
+/* Set mines */
+function setMines() {
+    /** @type {number} */
+    let minesLeft = $mineCountStore;
+
+    mineLocations = [];
+
+    while (minesLeft > 0) {
+
+        /* Get random row and column */
+        let row = Math.floor(Math.random() * $boardSizeStore);
+        let column = Math.floor(Math.random() * $boardSizeStore);
+
+        /* If the tile is not already a mine, set it as a mine */
+        if (!mineLocations.includes(`${row}-${column}`)) {
+            mineLocations.push(`${row}-${column}`);
+            --minesLeft;
+        }
+
+    }
+    console.log(mineLocations);
+}
+
+boardSizeStore.subscribe((value) => {
+    startGame();
+});
 
 </script>
 
 
 <div
     class="board"
-    style="--board-size: {'auto' + (" auto".repeat($boardSizeStore-1))}"
+    style="
+        --board-size: {`${500/$boardSizeStore}px` + (` ${500/$boardSizeStore}px`.repeat($boardSizeStore-1))}
+    "
 >
-{#each rows as row}
-    {#each columns as column}
+
+{#each board as row}
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    {#each row as tile}
         <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <!-- svelte-ignore a11y-no-static-element-interactions -->
         <div
-            class="tile"
-            id={`${row.toString()}-${column.toString()}`}
-            on:click={() => {console.log('Hi')}}
-        ></div>
+            class={tile.class}
+            id={tile.id}
+            on:click={() => {
+                let position = tile.id.split('-');
+                console.log(position)
+
+                const tileElement = document.getElementById(tile.id);
+
+                if (tileElement !== null && !tileElement.classList.contains('clicked')) {
+                    ++tilesClicked;
+                    tile.class += ' clicked';
+                    console.log(tile.class);
+                }
+                if (mineLocations.includes(tile.id)) {
+                    console.log('BOOM');
+
+                }
+            }}
+        >{tile.text}</div>
     {/each}
 {/each}
+
 </div>
 
 
@@ -45,6 +136,7 @@
         border-radius: 10px;
         
         margin: 0;
+        padding: 0;
 
         display: grid;
         row-gap: 0;
@@ -53,19 +145,27 @@
 
     }
 
-    .board :nth-child(2n-1) {
+    .board .tile.bomb {
+        background-color: red !important;
+    }
+    .board .tile.clicked:not(.bomb) {
+        background-color: #ddd;
+    }
+
+    .board :nth-child(2n-1).tile {
         background-color: #666;
     }
 
-    .board :nth-child(2n-1):hover {
+    .board :nth-child(2n-1).tile:hover {
         cursor: pointer;
         background-color: #777;
     }
-    .board :nth-child(2n) {
+    .board :nth-child(2n).tile {
         background-color: #aaa;
     }
-    .board :nth-child(2n):hover {
+    .board :nth-child(2n).tile:hover {
         cursor: pointer;
         background-color: #bbb;
     }
+
 </style>
--- src/stores.js
+++ src/stores.js
@@ -1,7 +1,10 @@
 import { writable } from "svelte/store";
 
+/** @type {import('svelte/store').Writable<number>} */
 export const boardSizeStore = writable(9);
+
+/** @type {import('svelte/store').Writable<number>} */
+export const mineCountStore = writable(5);
 
 /** @type {import('svelte/store').Writable<boolean>} */
 export const flagActiveStore = writable(false);
-
